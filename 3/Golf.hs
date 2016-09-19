4bb8901503b03dcc3f4a74a358d90a02889af03e
Bryce Hill <bryce@clearvoice.com>
2016-09-19 08:12:43 -0700

    week 3 ex 3

--- 3/Golf.hs
+++ 3/Golf.hs
@@ -1,5 +1,6 @@
 module Golf where
 
+import Data.List
 -- skips "ABCD" == ["ABCD", "BD", "C", "D"]
 -- skips "hello!" == ["hello!", "el!", "l!", "l", "o", "!"]
 -- skips [1] == [[1]]
@@ -19,3 +20,38 @@ everyNth n xs = [xs !! idx | idx <- [n-1, n-1+n..length xs - 1]]
 localMaxima :: [Int] -> [Int]
 localMaxima (x:y:z:zs) = (if y > x && y > z then [y] else []) ++ localMaxima (y:z:zs)
 localMaxima _ = []
+
+
+-- histogram [1,1,1,5] ==
+--  *
+--  *
+--  * *
+-- ==========
+-- 0123456789
+-- histogram [1,4,5,4,6,6,3,4,2,4,9] ==
+--  *
+--  *
+--  * *
+--  ****** *
+-- ==========
+-- 0123456789
+
+-- histogram [3,5]
+-- " * * \n==========\n0123456789\n
+
+-- putStr (histogram [3,5])
+
+
+histogram :: [Integer] -> String
+histogram ns = intercalate "\n" $
+  reverse (map replaceWithStar $ transpose
+   . group $ sort ns) ++ ["==========","0123456789\n"]
+
+
+replaceWithStar :: [Integer] -> String
+replaceWithStar = foldl (replace "*") spaces
+  where spaces = replicate 10 ' '
+
+
+replace :: String -> String -> Integer -> String
+replace newStr s n = take (fromIntegral n) s ++ newStr ++ drop (fromIntegral (n+1)) s
